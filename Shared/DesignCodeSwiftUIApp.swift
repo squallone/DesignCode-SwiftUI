e2c4cefa66204492352b4ce9f05b32bce32598bd
Abdiel Soto <asoto@etsy.com>
2021-08-15 22:38:24 -0500

    Initial setup

---
+++ Shared/DesignCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeSwiftUIApp.swift
+//  Shared
+//
+//  Created by Abdiel Soto on 8/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
