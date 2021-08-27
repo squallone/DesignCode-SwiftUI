61ac9a59aed46dbd8f59a7f1285fe146db452e5f
Abdiel Soto <asoto@etsy.com>
2021-08-26 23:00:08 -0500

    Add card for courses item

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+               Spacer()
+                Image("Illustration 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+            Text("SwiftUI for iOS 14")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            Text("20 sections")
+                .font(.footnote)
+                .foregroundColor(.white)
+        }
+        .padding(.all)
+        .background(Color.blue)
+        .cornerRadius(20.0)
+        .shadow(radius: 10)
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 200.0, height: 200.0))
+            .preferredColorScheme(.dark)
+            .environment(\.sizeCategory, .large)
+            .previewDevice("iPhone 8")
+
     }
 }
